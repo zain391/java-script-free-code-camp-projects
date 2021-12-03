b4c45f93e407ddcdeb52112fe4bd40f934347fe2
zain391 <zainqamar391@gmail.com>
2021-12-03 11:21:51 -0800

    toggle navbar project
    toggle navbar project
    forth project

---
+++ togglenav-bar/bar/script.js
@@ -0,0 +1,8 @@
+function show(){
+    let link=document.querySelector(".link");
+    let second=document.querySelector(".second");
+    let parent=document.querySelector(".parent");
+    link.classList.toggle("link2");
+    second.classList.toggle("link2");
+    parent.classList.toggle("link2");
+}
