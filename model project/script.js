c9407bd753f8b139c5c8ad05a05b4958b2fdcc77
zain391 <zainqamar391@gmail.com>
2021-12-07 18:12:18 -0800

    I have made the model project
    I have made the model project

---
+++ model project/script.js
@@ -0,0 +1,8 @@
+function btn1(){
+    let btn1=document.querySelector(".container2");
+    btn1.classList.toggle("container3");
+}
+function btn2(){
+    let btn1=document.querySelector(".container2");
+    btn1.classList.toggle("container3");
+}
