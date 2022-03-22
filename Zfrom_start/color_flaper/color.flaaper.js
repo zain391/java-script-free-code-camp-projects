5c09ee7da8f31136e7f4921c3e7e89bd4a08b19c
zain391 <zainqamar391@gmail.com>
2022-03-22 21:38:10 +0500

    we have made our first project which is changing background color of body
    we have made our first project which is changing background color of body

--- Zfrom_start/color_flaper/color.flaaper.js
+++ Zfrom_start/color_flaper/color.flaaper.js
@@ -1,15 +1,17 @@
 let btn=document.querySelector(".random");
 let color_text=document.querySelector('.color_text');
-let hex='#';
-let arrayOfColors=[1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+let arrayOfColors=[0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
 btn.addEventListener('click',function(){
+    let hex='#';
     for(let i=0; i<6 ; i++){
         hex+=arrayOfColors[getRandom()];
     }
+    
     document.body.style.backgroundColor=hex;
+    color_text.textContent=hex;
 });
-color_text.textContent=hex;
+// console.log(hex);
 function getRandom(){
-    return Math.floor(Math.random()*arrayOfColors.length);
+    return Math.floor(Math.random()*arrayOfColors.length-1);
 }
-getRandom();
+console.log(getRandom());
