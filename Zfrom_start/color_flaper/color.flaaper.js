5342b961dda3caace8622681e74078020e15ad98
zain391 <zainqamar391@gmail.com>
2022-03-22 20:50:00 +0500

    manually changed the background color of body
    manually changed the background color of body

--- Zfrom_start/color_flaper/color.flaaper.js
+++ Zfrom_start/color_flaper/color.flaaper.js
@@ -1,2 +1,7 @@
 let btn=document.querySelector(".random");
 console.log(btn);
+let color_text=document.querySelector('.color_text');
+console.log(color_text);
+let arrayOfColors=['green','red','blue'];
+let randomNumber=2;
+document.body.style.backgroundColor=arrayOfColors[randomNumber];
