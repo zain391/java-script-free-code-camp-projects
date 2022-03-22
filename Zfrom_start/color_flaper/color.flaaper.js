b16dd5ae37d47c37aa38c3b5ac9498b2d162d460
zain391 <zainqamar391@gmail.com>
2022-03-22 21:14:50 +0500

    these are all the concepts we needed to make our first project
    these are all the concepts we needed to make our first project

--- Zfrom_start/color_flaper/color.flaaper.js
+++ Zfrom_start/color_flaper/color.flaaper.js
@@ -1,7 +1,12 @@
 let btn=document.querySelector(".random");
 console.log(btn);
+// now we need to get the text 
 let color_text=document.querySelector('.color_text');
 console.log(color_text);
 let arrayOfColors=['green','red','blue'];
 let randomNumber=2;
 document.body.style.backgroundColor=arrayOfColors[randomNumber];
+color_text.textContent=arrayOfColors[randomNumber];
+function getRandom(){
+    return Math.floor(Math.random()*3);
+}
